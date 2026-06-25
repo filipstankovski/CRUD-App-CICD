6d53ef2c696d2c07d8f1ade174dc7122dd35468a
filipstankovski <filipstankovski.contact@gmail.com>
2026-06-26 00:53:21 +0200

    Configure frontend for Kubernetes with Nginx reverse proxy

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "/api/v1/employees";
 
 class EmployeeService {
 
@@ -13,16 +13,16 @@ class EmployeeService {
     }
 
     getEmployeeById(employeeId){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+        return axios.get(`${EMPLOYEE_API_BASE_URL}/${employeeId}`);
     }
 
     updateEmployee(employee, employeeId){
-        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+        return axios.put(`${EMPLOYEE_API_BASE_URL}/${employeeId}`, employee);
     }
 
     deleteEmployee(employeeId){
-        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+        return axios.delete(`${EMPLOYEE_API_BASE_URL}/${employeeId}`);
     }
 }
 
-export default new EmployeeService()
+export default new EmployeeService();
